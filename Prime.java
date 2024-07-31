05a499dcc78f130dbb7c3f7fb2c25f90941b9f91
dac <dac@students.acts.in>
2024-07-31 16:26:10 +0530

    first commit

---
+++ Prime.java
@@ -0,0 +1,8 @@
+public class Prime{
+    public static void main(String[] args) {
+        System.out.println("First 10 even numbers:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * 2);
+        }
+    }
+}
